9ed3a1e4ccb52bb53bab811de99a27ba2c6d0647
Rania-Helmy <ranmhelmy@gmail.com>
2023-03-27 10:27:09 +0300

    Update ScrollTop Functionality

--- src/app/Components/scroll-top/scroll-top.component.ts
+++ src/app/Components/scroll-top/scroll-top.component.ts
@@ -10,9 +10,32 @@ declare var jQuery: any;
 export class ScrollTopComponent {
   ngAfterViewInit() {
     $(document).ready(function () {
+      $(window).on('load', function() {
+        // let scrollTop = document.documentElement.scrollTop;
+        // if (scrollTop === 0) {
+        //   $('#scrollTop').addClass("d-none");
+        // } else {
+        //   $('#scrollTop').removeClass("d-none");
+        // }
+        var scrollDistance = $(window).scrollTop();
+        if (scrollDistance > 100) {
+          $('#scrollTop').removeClass("d-none");
+          $('.scroll-to-top').fadeIn();
+        } else {
+          // $('.scroll-to-top').fadeOut();
+        }
+
+      });
+
+      $('#scrollTop').on('click', function() {
+        console.log('hi');
+        $(window).scrollTop(0);
+      })
+
       $(document).scroll(function () {
         var scrollDistance = $(window).scrollTop();
         if (scrollDistance > 100) {
+          $('#scrollTop').removeClass("d-none");
           $('.scroll-to-top').fadeIn();
         } else {
           $('.scroll-to-top').fadeOut();
